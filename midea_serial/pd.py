2245d624bee6b408c50bcf0638af29447527080a
David <dav.lobato@gmail.com>
2020-07-27 17:35:13 +0100

    Cleanup

--- midea_serial/pd.py
+++ midea_serial/pd.py
@@ -23,17 +23,19 @@ RX = 0
 TX = 1
 rxtx_channels = ('RX', 'TX')
 
+HEADER_LENGTH = 8  # sync byte not included
+LENGTH_OFFSET = 0
 APPLIANCE_TYPE_OFFSET = 1
 MSG_ID_OFFSET = 5
+FRAMEWORK_VERSION_OFFSET = 6
+APPLIANCE_VERSION_OFFSET = 7
 MSG_TYPE_OFFSET = 8
 MSG_BODY_OFFSET = 9
 
 
 # message bits from http://chipsc.com/home/views/default/resource/images/111.pdf [1]
 class DecoderState:
-    IDLE, READ_MESSAGE_LENGTH, READ_APPLIANCE_TYPE, READ_FRAME_SYNC_CHECK, READ_KEEP, \
-     READ_MESSAGE_ID, READ_FRAMEWORK_AGREEMENT_VERSION, READ_APPLIANCE_AGREEMENT_VERSION, \
-     READ_MESSAGE_TYPE, READ_MESSAGE_BODY, READ_CHECKSUM = range(11)
+    IDLE, READ_HEADER, READ_MESSAGE = range(3)
 
 
 class Decoder(srd.Decoder):
@@ -49,22 +51,20 @@ class Decoder(srd.Decoder):
     annotations = (
         ('am-sync', 'AM sync'),  # 0
         ('am-header', 'AM header'),  # 1
-        ('am-body', 'AM body'),  # 2
-        ('am-checksum', 'AM checksum'),  # 3
-        ('ma-sync', 'MA sync'),  # 4
-        ('ma-header', 'MA header'),  # 5
-        ('ma-body', 'MA body'),  # 6
-        ('ma-checksum', 'MA checksum'),  # 7
-        ('am-cmd', 'AM Cmd'),  # 8
-        ('ma-cmd', 'MA Cmd'),  # 9
-        ('error-indication', 'Error indication'),  # 10
+        ('am-msg', 'AM msg'),  # 2
+        ('ma-sync', 'MA sync'),  # 3
+        ('ma-header', 'MA header'),  # 4
+        ('ma-msg', 'MA msg'),  # 5
+        ('am-cmd', 'AM Cmd'),  # 6
+        ('ma-cmd', 'MA Cmd'),  # 7
+        ('error-indication', 'Error indication'),  # 8
     )
-    bytes_annotations_stride = 4
+    bytes_annotations_stride = 3
     annotation_rows = (
-        ('am', 'Appliance->Module', tuple(range(4))),
-        ('ma', 'Module->Appliance', tuple(range(4, 8))),
-        ('req-res', 'Request/Response', (8, 9)),
-        ('error-indicators', 'Errors in frame', (10,)),
+        ('am', 'Appliance->Module', tuple(range(3))),
+        ('ma', 'Module->Appliance', tuple(range(3, 6))),
+        ('req-res', 'Request/Response', (6, 7)),
+        ('error-indicators', 'Errors in frame', (8,)),
     )
 
     def __init__(self):
@@ -72,10 +72,9 @@ class Decoder(srd.Decoder):
 
     def reset(self):
         self.state = [DecoderState.IDLE, DecoderState.IDLE]
-        self.msg = [[], []]
-        self.ss_msg_body_block = [None, None]
-        self.data_lenght = [0, 0]
-        self.data_read = [0, 0]
+        self.data = [[], []]
+        self.ss_header_block = [None, None]
+        self.ss_msg_block = [None, None]
 
     def start(self):
         self.out_ann = self.register(srd.OUTPUT_ANN)
@@ -88,87 +87,63 @@ class Decoder(srd.Decoder):
             return
 
         if self.state[rxtx] != DecoderState.IDLE:
-            self.msg[rxtx].append(pdata[0])
-            self.data_read[rxtx] += 1
+            self.data[rxtx].append(pdata[0])
 
         if self.state[rxtx] == DecoderState.IDLE:
             if pdata[0] == 0xAA:  # 0xAA sync header
-                self.msg[rxtx] = []
-                self.data_read[rxtx] = 0
-                self.ss_msg_body_block[rxtx] = None
+                self.data[rxtx] = []
+                self.ss_header_block[rxtx] = None
+                self.ss_msg_block[rxtx] = None
 
-                self.state[rxtx] = DecoderState.READ_MESSAGE_LENGTH
+                self.state[rxtx] = DecoderState.READ_HEADER
 
                 self.put(ss, es, self.out_ann, [0 + (rxtx * Decoder.bytes_annotations_stride), ['Sync']])
-        elif self.state[rxtx] == DecoderState.READ_MESSAGE_LENGTH:
-            self.data_lenght[rxtx] = pdata[0]
-            self.state[rxtx] = DecoderState.READ_APPLIANCE_TYPE
+        elif self.state[rxtx] == DecoderState.READ_HEADER:
+            if self.ss_header_block[rxtx] is None:
+                self.ss_header_block[rxtx] = ss
 
-            self.put(ss, es, self.out_ann,
-                     [1 + (rxtx * Decoder.bytes_annotations_stride), ['L: {:d}'.format(self.data_lenght[rxtx])]])
-        elif self.state[rxtx] == DecoderState.READ_APPLIANCE_TYPE:
-            self.state[rxtx] = DecoderState.READ_FRAME_SYNC_CHECK
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['At: {:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_FRAME_SYNC_CHECK:
-            self.state[rxtx] = DecoderState.READ_KEEP
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['Fs: {:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_KEEP:
-            if self.data_read[rxtx] == 5:
-                self.state[rxtx] = DecoderState.READ_MESSAGE_ID
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['{:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_MESSAGE_ID:
-            self.state[rxtx] = DecoderState.READ_FRAMEWORK_AGREEMENT_VERSION
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['Mid:{:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_FRAMEWORK_AGREEMENT_VERSION:  # according to [1] not used
-            self.state[rxtx] = DecoderState.READ_APPLIANCE_AGREEMENT_VERSION
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['Fv:{:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_APPLIANCE_AGREEMENT_VERSION:  # according to [1] not used
-            self.state[rxtx] = DecoderState.READ_MESSAGE_TYPE
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['Av:{:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_MESSAGE_TYPE:
-            self.state[rxtx] = DecoderState.READ_MESSAGE_BODY
-            self.put(ss, es, self.out_ann, [1 + (rxtx * Decoder.bytes_annotations_stride), ['Mt:{:02X}'.format(pdata[0])]])
-        elif self.state[rxtx] == DecoderState.READ_MESSAGE_BODY:
-            if self.ss_msg_body_block[rxtx] is None:
-                self.ss_msg_body_block[rxtx] = ss
+            if len(self.data[rxtx]) == HEADER_LENGTH:
+                self.state[rxtx] = DecoderState.READ_MESSAGE
+                self.put(self.ss_header_block[rxtx], es, self.out_ann,
+                         [1 + (rxtx * Decoder.bytes_annotations_stride), [
+                             'L: {:d}, At: {:02X}, Mid:{:02X}, v:{:02X}{:02X}'.format(self.data[rxtx][LENGTH_OFFSET],
+                                                                                      self.data[rxtx][APPLIANCE_TYPE_OFFSET],
+                                                                                      self.data[rxtx][MSG_ID_OFFSET],
+                                                                                      self.data[rxtx][FRAMEWORK_VERSION_OFFSET],
+                                                                                      self.data[rxtx][APPLIANCE_VERSION_OFFSET])]])
+        elif self.state[rxtx] == DecoderState.READ_MESSAGE:
+            if self.ss_msg_block[rxtx] is None:
+                self.ss_msg_block[rxtx] = ss
 
-            if self.data_read[rxtx] == (self.data_lenght[rxtx] - 1):
-                self.state[rxtx] = DecoderState.READ_CHECKSUM
-
-                self.put(self.ss_msg_body_block[rxtx], es, self.out_ann,
+            if len(self.data[rxtx]) == self.data[rxtx][LENGTH_OFFSET]:
+                self.state[rxtx] = DecoderState.IDLE
+                msg_body = ''.join(['{:02X}'.format(n) for n in self.data[rxtx][MSG_BODY_OFFSET:-1]])
+                self.put(self.ss_msg_block[rxtx], es, self.out_ann,
                          [2 + (rxtx * Decoder.bytes_annotations_stride),
-                          [' '.join(['{:02X}'.format(n) for n in self.msg[rxtx][MSG_BODY_OFFSET:]]), 'Data']])
-                msg_type = self.msg[rxtx][MSG_TYPE_OFFSET]
-                cmd_handler = getattr(self, 'cmd_handler_0x{:02X}'.format(msg_type), self.cmd_handler_unknown)
-                cmd_handler(self.ss_msg_body_block[rxtx], es, (rxtx, self.msg[rxtx][APPLIANCE_TYPE_OFFSET], msg_type, self.msg[rxtx][MSG_BODY_OFFSET:]))
-        elif self.state[rxtx] == DecoderState.READ_CHECKSUM:
-            self.state[rxtx] = DecoderState.IDLE
+                          ['Type: {:02X}, {}, Checksum: {:02X}'.format(self.data[rxtx][MSG_TYPE_OFFSET], msg_body,
+                                                                       self.data[rxtx][-1])]])
+                if checksum(self.data[rxtx][MSG_BODY_OFFSET:]) != 0:
+                    msg_type = self.data[rxtx][MSG_TYPE_OFFSET]
+                    cmd_handler = getattr(self, 'cmd_handler_0x{:02X}'.format(msg_type), self.cmd_handler_default)
+                    cmd_handler(self.ss_msg_block[rxtx], es, (rxtx, self.data[rxtx]))
+                else:
+                    self.put(ss, es, self.out_ann, [8, ['Checksum failed']])
 
-            self.put(ss, es, self.out_ann, [3 + (rxtx * Decoder.bytes_annotations_stride), ['Csum: {:02X}'.format(pdata[0])]])
-            if checksum(self.msg[rxtx]) != 0:
-                self.put(ss, es, self.out_ann, [10, ['Checksum failed']])
-            data_str = ''.join(['{:02X}'.format(n) for n in self.msg[rxtx][APPLIANCE_TYPE_OFFSET:-1]])
-            print('RXTX[{}]: L={} D={}'.format(rxtx, self.data_lenght[rxtx], data_str))
+                data_str = ''.join(['{:02X}'.format(n) for n in self.data[rxtx][APPLIANCE_TYPE_OFFSET:-1]])
+                print('RXTX[{}]: L={} D={}'.format(rxtx, self.data[rxtx][LENGTH_OFFSET], data_str))
 
-    def cmd_handler_unknown(self, ss, es, data):
-        rxtx, appliance_type, msg_type, msg_body = data
+    def cmd_handler_default(self, ss, es, data):
+        rxtx, read_data = data
 
-        self.put(ss, es, self.out_ann, [8 + rxtx, ['Unknown msg type 0x{:02X}'.format(msg_type)]])
-
-    def cmd_handler_0x03(self, ss, es, data):
-        rxtx, appliance_type, msg_type, msg_body = data
-
-        crc_status = 'OK' if crc8(msg_body) == 0 else 'Error'
-
-        self.put(ss, es, self.out_ann, [8 + rxtx, ['msg type 0x03 CRC {}'.format(crc_status)]])
+        self.put(ss, es, self.out_ann, [6 + rxtx, ['msg type 0x{:02X}'.format(read_data[MSG_TYPE_OFFSET])]])
 
     def cmd_handler_0x63(self, ss, es, data):
-        rxtx, appliance_type, msg_type, msg_body = data
+        rxtx, read_data = data
 
-        ann = None
         if rxtx == 0:  # request from appliance
-            ann = ['Query network status']
+            self.put(ss, es, self.out_ann, [6 + rxtx, ['Query network status']])
         else:
+            msg_body = read_data[MSG_BODY_OFFSET:]
             module_type = 'RF' if msg_body[0] == 0 else 'Wi-Fi'
             module_mode = msg_body[1]
             wifi_signal_strength = msg_body[2]
@@ -178,5 +153,8 @@ class Decoder(srd.Decoder):
             cloud_status = 'OK' if msg_body[9] == 0 else msg_body[9]
             conn_status = msg_body[10]
             tcp_conn_count = msg_body[11]
-            ann = ['{}({}), IP: {}.{}.{}.{}, Net Status: {}, Cloud status: {}'.format(module_type, module_mode, ip_address[3], ip_address[2], ip_address[1], ip_address[0], network_status, cloud_status)]
-        self.put(ss, es, self.out_ann, [8 + rxtx, ann])
+            self.put(ss, es, self.out_ann, [6 + rxtx, [
+                '{}({}), IP: {}.{}.{}.{}, Net Status: {}, Cloud status: {}'.format(module_type, module_mode,
+                                                                                   ip_address[3], ip_address[2],
+                                                                                   ip_address[1], ip_address[0],
+                                                                                   network_status, cloud_status)]])
